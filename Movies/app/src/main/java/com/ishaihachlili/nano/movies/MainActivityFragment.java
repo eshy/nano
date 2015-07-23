f78c52cbd93f094f64059571fd941a0ba0bc2692
Ishai Hachlili <ishaih@eshymedia.com>
2015-07-23 14:50:11 -0700

    Main screen done (displaying posters, sorting settings)

--- Movies/app/src/main/java/com/ishaihachlili/nano/movies/MainActivityFragment.java
+++ Movies/app/src/main/java/com/ishaihachlili/nano/movies/MainActivityFragment.java
@@ -17,7 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.GridView;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,7 +38,7 @@ import java.util.ArrayList;
  */
 public class MainActivityFragment extends Fragment {
 
-    private ArrayAdapter<String> mMoviesAdapter;
+    private MoviesArrayAdapter mMoviesAdapter;
 
     public MainActivityFragment() {
     }
@@ -67,25 +68,20 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        mMoviesAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_movie, // The name of the layout ID.
-                        R.id.list_item_movie_textview, // The ID of the textview to populate.
-                        new ArrayList<String>());
+        mMoviesAdapter =new MoviesArrayAdapter(getActivity(), R.layout.list_item_movie);
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         // Get a reference to the ListView, and attach this adapter to it.
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_movies);
-        listView.setAdapter(mMoviesAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridView.setAdapter(mMoviesAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String forecast = mMoviesAdapter.getItem(position);
+                Integer movieId = mMoviesAdapter.getItem(position)._movieId;
                 Intent intent = new Intent(getActivity(), DetailActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                        .putExtra(Intent.EXTRA_TEXT, movieId);
                 startActivity(intent);
             }
         });
@@ -107,11 +103,11 @@ public class MainActivityFragment extends Fragment {
         updateMovies();
     }
 
-    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+    public class FetchMoviesTask extends AsyncTask<String, Void, MovieModel[]> {
         private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
 
         @Override
-        protected String[] doInBackground(String... params) {
+        protected MovieModel[] doInBackground(String... params) {
             if (params.length == 0) {
                 return null;
             }
@@ -119,7 +115,7 @@ public class MainActivityFragment extends Fragment {
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
-            String moviesJsonStr = null;
+            String moviesJson = null;
             String apikey = "123456";
             ///discover/movie?sort_by=popularity.desc
             try {
@@ -160,7 +156,7 @@ public class MainActivityFragment extends Fragment {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                moviesJsonStr = buffer.toString();
+                moviesJson = buffer.toString();
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 return null;
@@ -178,7 +174,7 @@ public class MainActivityFragment extends Fragment {
             }
 
             try {
-                return getMoviesDataFromJson(moviesJsonStr,10);
+                return getMoviesDataFromJson(moviesJson);
             } catch (JSONException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
@@ -189,85 +185,34 @@ public class MainActivityFragment extends Fragment {
 
         }
 
-        private String[] getMoviesDataFromJson(String forecastJsonStr, int numDays)
+        private MovieModel[] getMoviesDataFromJson(String moviesJson)
                 throws JSONException {
 
             // These are the names of the JSON objects that need to be extracted.
-            final String OWM_LIST = "list";
-            final String OWM_WEATHER = "weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX = "max";
-            final String OWM_MIN = "min";
-            final String OWM_DESCRIPTION = "main";
+            final String TMDB_LIST = "results";
+            final String TMDB_POSTERPATH = "poster_path";
+            final String TMDB_MOVIEID = "id";
 
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+            JSONObject moviesJsonObject = new JSONObject(moviesJson);
+            JSONArray moviesArray = moviesJsonObject.getJSONArray(TMDB_LIST);
 
-            // OWM returns daily forecasts based upon the local time of the city that is being
-            // asked for, which means that we need to know the GMT offset to translate this data
-            // properly.
+            MovieModel[] resultMovies = new MovieModel[moviesArray.length()];
 
-            // Since this data is also sent in-order and the first day is always the
-            // current day, we're going to take advantage of that to get a nice
-            // normalized UTC date for all of our weather.
-
-            Time dayTime = new Time();
-            dayTime.setToNow();
-
-            // we start at the day returned by local time. Otherwise this is a mess.
-            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-            // now we work exclusively in UTC
-            dayTime = new Time();
-
-            String[] resultStrs = new String[numDays];
-
-            // Data is fetched in Celsius by default.
-            // If user prefers to see in Fahrenheit, convert the values here.
-            // We do this rather than fetching in Fahrenheit so that the user can
-            // change this option without us having to re-fetch the data once
-            // we start storing the values in a database.
-
-            for(int i = 0; i < weatherArray.length(); i++) {
-                // For now, using the format "Day, description, hi/low"
-                String day;
-                String description;
-                String highAndLow;
-
-                // Get the JSON object representing the day
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                // The date/time is returned as a long.  We need to convert that
-                // into something human-readable, since most people won't read "1400356800" as
-                // "this saturday".
-                long dateTime;
-                // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay + i);
-                //day = getReadableDateString(dateTime);
-
-                // description is in a child array called "weather", which is 1 element long.
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                description = weatherObject.getString(OWM_DESCRIPTION);
-
-                // Temperatures are in a child object called "temp".  Try not to name variables
-                // "temp" when working with temperature.  It confuses everybody.
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble(OWM_MAX);
-                double low = temperatureObject.getDouble(OWM_MIN);
-
-                //highAndLow = formatHighLows(high, low, unitType);
-                resultStrs[i] = description;
+            for(int i = 0; i < moviesArray.length(); i++) {
+                // Get the JSON object representing the movie
+                JSONObject movie = moviesArray.getJSONObject(i);
+                resultMovies[i] = new MovieModel(movie.getString(TMDB_POSTERPATH), movie.getInt(TMDB_MOVIEID));
             }
-            return resultStrs;
+            return resultMovies;
 
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(MovieModel[] result) {
             if (result != null) {
                 mMoviesAdapter.clear();
-                for(String moviePosterUrl : result) {
-                    mMoviesAdapter.add(moviePosterUrl);
+                for(MovieModel movie : result) {
+                    mMoviesAdapter.add(movie);
                 }
             }
         }
---
+++ Movies/app/src/main/java/com/ishaihachlili/nano/movies/MoviesArrayAdapter.java
@@ -0,0 +1,36 @@
+package com.ishaihachlili.nano.movies;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Ishai on 7/23/2015.
+ */
+public class MoviesArrayAdapter extends ArrayAdapter<MovieModel> {
+    private final Activity _context;
+
+    public MoviesArrayAdapter(Activity context, int resource) {
+        super(context, resource);
+        _context=context;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater = _context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.list_item_movie, null, true);
+        MovieModel movie = getItem(position);
+        if (movie != null) {
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.poster);
+            String posterPath = movie.getMoviePath();
+            Picasso.with(_context).load(posterPath).into(imageView);
+        }
+
+        return rowView;
+    };
+}
